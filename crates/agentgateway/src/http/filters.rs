020fc6b10d99dcf21926f576edb31cc49b1376db
John Howard <john.howard@solo.io>
2025-08-18 12:34:50 -0700

    Fix status code deserialization

--- crates/agentgateway/src/http/filters.rs
+++ crates/agentgateway/src/http/filters.rs
@@ -52,12 +52,7 @@ pub struct RequestRedirect {
 	pub authority: Option<HostRedirect>,
 	#[serde(skip_serializing_if = "is_default")]
 	pub path: Option<PathRedirect>,
-	#[serde(
-		default,
-		skip_serializing_if = "is_default",
-		serialize_with = "ser_display_option",
-		deserialize_with = "de_parse_option"
-	)]
+	#[serde(default, with = "http_serde::option::status_code")]
 	#[cfg_attr(feature = "schema", schemars(with = "Option<std::num::NonZeroU16>"))]
 	pub status: Option<http::StatusCode>,
 }
